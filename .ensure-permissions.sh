207d48b5b5b4aa1978764f27d44d6da807939380
Aria Buckles <aria@toole1.com>
2019-11-13 17:11:13 -0800

    Update .ensure-permissions script

--- .ensure-permissions.sh
+++ .ensure-permissions.sh
@@ -4,13 +4,15 @@ if [[ "$USER" == "root" ]]; then
   chown -PR aria ~aria/home
   chown -PR aria ~aria/bin
   chown -PR aria ~aria/Documents
-  find -P ~aria/shell -type l | xargs chown -h aria
-  find -P ~aria/shell -type d | xargs chown -PRh ariashell
+  chown aria ~aria/shell
+  find -P ~aria/shell -type l -print0 | xargs -0 chown -h aria
+  find -P ~aria/shell -type d -print0 | xargs -0 chown -PRh ariashell
 fi
 if [[ "$USER" != "ariashell" ]]; then
   chmod -R u=rwX,g=rX,o= ~aria/home
   chmod -R u=rwX,g=rX,o= ~aria/bin
   chmod 700 ~aria/Documents
+  chmod 750 ~aria/shell
 fi
-find -P ~aria/shell -type l | xargs chmod -h 640
-find -P ~aria/shell -type d | xargs chmod -R ug+rX
+find -P ~aria/shell -type l -print0 | xargs -0 chmod -h 640
+find -P ~aria/shell -type d -print0 | xargs -0 chmod -R ug+rX
