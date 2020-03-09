f0e8be272be3b6189e9cee0957dbb9961bc46a7c
Aria Buckles <aria@pilot.com>
2020-03-09 14:42:07 -0700

    Add vifd and vifa support for only opening files changed since master

--- .zsh/hooks/nvim+fzf.sh
+++ .zsh/hooks/nvim+fzf.sh
@@ -9,5 +9,18 @@ vif() {
   fi
 }
 vifa() {
-  nvim -p -- $(fzf -f "$*")
+  if [[ "$#" -gt 0 ]]; then
+    nvim -p -- $(fzf -f "$*")
+  else
+    nvim -p -- $(git diff --relative --name-only master...HEAD)
+  fi
+}
+vifd() {
+  if [[ "$#" -gt 0 ]]; then
+    file=$(git diff --relative --name-only master...HEAD | fzf --preview='bat --style=full --color=always {}' -1 -q "$*")
+    [[ -n "$file" ]] && nvim -- "$file"
+  else
+    file=$(git diff --relative --name-only master...HEAD | fzf --preview='bat --style=full --color=always {}')
+    [[ -n "$file" ]] && nvim -- "$file"
+  fi
 }
