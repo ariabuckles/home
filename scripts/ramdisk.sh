a20cc6c95ce4d1c1930296396709bf50f6d92d87
Aria Buckles <aria@toole1.com>
2021-01-21 22:55:02 -0800

    Ramdisk: disable suid
    Also clean up the shell script a bit

--- scripts/ramdisk.sh
+++ scripts/ramdisk.sh
@@ -1,11 +1,11 @@
-#!/bin/zsh
+#!/bin/sh
 # Makes a ramdisk for development:
 sudo mkdir -p /mnt/ramdisk
 if ! grep -q ' /mnt/ramdisk ' /etc/fstab; then
   echo "# Aria: ramdisk for development:" | sudo tee -a /etc/fstab
-  echo "tmpfs /mnt/ramdisk tmpfs defaults,size=32g" | sudo tee -a /etc/fstab
+  echo "tmpfs /mnt/ramdisk tmpfs defaults,nosuid,nodev,size=32g" | sudo tee -a /etc/fstab
 fi
 sudo mount -a
-mkdir -p --mode=700 /mnt/ramdisk/src
+mkdir --mode=700 /mnt/ramdisk/src
 mkdir -p ~/.src
 ln -snf /mnt/ramdisk/src ~/src
